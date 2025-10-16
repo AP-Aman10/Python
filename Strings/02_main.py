85eecdfda16a678566a8b94dc322cf233657dbf5
Aman Patidar <amanpatidar812@gmail.com>
2025-10-16 15:27:44 +0530

    Add files via upload

---
+++ Strings/02_main.py
@@ -0,0 +1,6 @@
+name = "Aman Patidar"
+
+print(f"Length of name: {len(name)}")
+print(f"Does the name end with 'idar'? {name.endswith('idar')}")
+print(f"Does the name start with 'Am'? {name.startswith('Am')}")
+print(f"Capitalized name: {name.capitalize()}")
---
+++ Strings/03_main.py
@@ -0,0 +1,4 @@
+String = 'Aman is a good boy\nbut not a bad \'boy\''
+
+
+print(String)
